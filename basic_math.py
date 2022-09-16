c7002e3c9a3c7939e3fce0120dda19b7f1da2c90
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2022-09-16 18:34:39 +0000

    “Commit”

---
+++ basic_math.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created By: Kestrel Bryce
+# Date: Sep. 16, 2022
+# This program does basic math
+
+
+def main():
+  # this function does basic math
+  print(9+2)
+  print("5-2={}".format(5-2))
+  print("3+15={}".format(3+15))
+  print("7*3={}".format(7*3))
+  print("5**2={}".format(5**2))
+  print("5/2={}".format(5/2))
+  print("40/2**3={}".format(40/2**3))
+  print("56%5={}".format(56%5))
+  # print("The square root of 25 is {}".format())
+
+
+if __name__ == "__main__":
+    main()
+  

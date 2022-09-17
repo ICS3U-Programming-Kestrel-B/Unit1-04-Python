5bff2479135d4265e3edbcb0097f0ca4dd95fe1f
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2022-09-17 19:53:09 +0000

    “Coded”

---
+++ area_per_rect.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created By: Kestrel Bryce
+# Date: Sep. 17, 2022
+# This program calculates the area and perimeter of a rectangle
+#   with 9cm x 4cm dimensions
+
+
+def main():
+  # this function calculates the area and perimeter
+  # print("If a rectangle has the dimensions:")
+  print("9cm x 4cm")
+  print("")
+  print("Area is {}cm^2".format(9*4))
+  print("Perimeter is {}cm".format(2*(9+4)))
+
+if __name__ == "__main__":
+  main()
